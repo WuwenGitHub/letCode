0c2c6a0a928741905aa94655aa1202db47f46d00
WuwenGitHub <1466358244@qq.com>
2018-04-02 10:59:54 +0800

    Create test.java

---
+++ test.java
@@ -0,0 +1,13 @@
+package letCode;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] height = {1, 0, -1, 0, -2, 2};
+		
+		System.out.println(new Solution().fourSum(height, 0));
+	}
+
+}
